cbbfbd82fb6bd9e08047b62ab2462884e3acc61b
Graeme Chalmers <chalmers.graeme@gmail.com>
2020-09-23 20:00:23 +0000

    updated play.js with UI

--- play.js
+++ play.js
@@ -1,3 +1,20 @@
 const {connect} = require('./client');
 console.log('Connecting ...');
 connect();
+
+const handleUserInput = function() {
+  stdin.on('data', (data) => {
+    if(data == '\\q\n') {
+      process.exit();
+    }
+    client.write(data);
+  })
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+};
